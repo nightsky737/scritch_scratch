fixed an issue with mse computation in my autodiff model

basics/model.py
@@ -205,7 +205,7 @@ class Model():
                 start_time = time.perf_counter() 
 
             # print(pred.shape) #32, 10
-            mse = np.sum(pred * pred - 2 * pred * y + y * y) #/(len(pred) * len(y))
+            mse = np.sum(pred * pred - 2 * pred * y + y * y) /(len(pred))
             num_correct += np.sum(np.argmax(pred, axis=1) == np.argmax(y[i], axis=1))
             losses.append(mse.data)
 

basics/tester.py
@@ -5,6 +5,7 @@ from model import *
 import numpy as np
 import jax.numpy as jnp
 import jax
+from jaxmodel import * 
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data(path="mnist.npz", )
 indices = np.arange(len(x_train))
@@ -24,12 +25,12 @@ def fix_data(x, y):
     test[np.arange(x.shape[0]),y] = 1
     return (x, test)
 
-fixed_x, fixed_y = fix_data(x_train[:1], y_train[:1])
-b_x , b_y = batch(fixed_x, fixed_y, 1)
+fixed_x, fixed_y = fix_data(x_train[:10000], y_train[:10000])
+b_x , b_y = batch(fixed_x, fixed_y, 32)
 
-my_model = Model(28*28, 10, [ 8, 16])
+my_model = JaxModel(28*28, 10, [ 8, 16], mse)
 datas = []
-for _epoch in range(100):
+for _epoch in range(20):
     print(f"starting epoch {_epoch}")
-    datas.append(my_model.train_epoch(b_x, b_y, lr=1e-2, timer=False, batch_timer=False))
+    datas.append(my_model.train_epoch(b_x, b_y, lr=1e-2 ))
 