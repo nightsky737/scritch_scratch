0f008d40c83068fa25e3d6153471b52b2795238a
nightsky737 <jseto737@gmail.com>
2025-07-25 17:00:05 -0400

    last commit hopefully everything works and i didnt forget a requirement

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -76,7 +76,7 @@ def loss_static(weights, x, y):
     y = jnp.array(y)
     return optax.softmax_cross_entropy(x, y).mean() #softmax_ce takes logits as the arg.
 
-# @jax.jit
+@jax.jit
 def fd(weights, x):
     k, b, ffnn = weights
     for i in range(len(k)):
